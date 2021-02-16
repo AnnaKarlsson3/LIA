3956d0c95cab2c164a7a30c49d5294eb15443a4e
AnnaKarlsson3 <zoora1234@hotmail.com>
2021-02-16 09:42:01 +0100

    register user spring
    wip register user,
    simple register

--- lia_spring/src/main/java/com/example/demo/controllers/UserController.java
+++ lia_spring/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,7 +1,21 @@
 package com.example.demo.controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.entities.User;
+import com.example.demo.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/rest")
 public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/user")
+    public ResponseEntity registerUser(@RequestBody User user){
+        userService.registerUser(user);
+        return ResponseEntity.ok(user);
+    }
 }
--- lia_spring/src/main/java/com/example/demo/services/UserService.java
+++ lia_spring/src/main/java/com/example/demo/services/UserService.java
@@ -1,7 +1,22 @@
 package com.example.demo.services;
 
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    //private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+
+    @Autowired
+    UserRepo userRepo;
+
+    public void registerUser(User user){
+        //String password = encoder.encode(user.getPassword());
+        //user.setPassword(password);
+        userRepo.save(user);
+    }
 }
