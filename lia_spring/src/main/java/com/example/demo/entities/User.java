f173c54df4ac0c1f7f091f0d42e74e1a195c2387
AnnaKarlsson3 <43141496+AnnaKarlsson3@users.noreply.github.com>
2021-02-19 18:23:35 +0100

    fixed some of the issuse in pom

--- lia_spring/src/main/java/com/example/demo/entities/User.java
+++ lia_spring/src/main/java/com/example/demo/entities/User.java
@@ -2,6 +2,7 @@ package com.example.demo.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +18,7 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @ApiModelProperty(value = "id", hidden = true)
     private int id;
     private String username;
     private String email;
