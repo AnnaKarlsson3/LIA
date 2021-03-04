e63457290dd940f91d3a02e7400a927a939081ab
AnnaKarlsson3 <43141496+AnnaKarlsson3@users.noreply.github.com>
2021-03-04 11:20:01 +0100

    removed a dependency

--- lia_micronaut/src/main/java/com/example/repositories/UserRepo.java
+++ lia_micronaut/src/main/java/com/example/repositories/UserRepo.java
@@ -1,13 +1,16 @@
 package com.example.repositories;
 
 import com.example.entities.User;
+import io.micronaut.context.annotation.Executable;
 import io.micronaut.data.annotation.Repository;
 import io.micronaut.data.repository.CrudRepository;
 
 
 @Repository
 public interface UserRepo extends CrudRepository<User, Integer> {
+    @Executable
     User findByEmail(String email);
+    @Executable
     User findById(int id);
 }
 
--- lia_micronaut/src/main/java/com/example/services/UserService.java
+++ lia_micronaut/src/main/java/com/example/services/UserService.java
@@ -3,11 +3,13 @@ package com.example.services;
 
 import com.example.entities.User;
 import com.example.repositories.UserRepo;
+import io.micronaut.core.annotation.Internal;
 
 import javax.inject.Inject;
+import javax.inject.Singleton;
 import java.util.List;
 
-
+@Singleton
 public class UserService {
 
     //private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
