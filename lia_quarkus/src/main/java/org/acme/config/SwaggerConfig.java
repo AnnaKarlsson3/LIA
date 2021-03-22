440d74c0e11c88045184d37adf385833d30c7e87
AnnaKarlsson3 <43141496+AnnaKarlsson3@users.noreply.github.com>
2021-03-22 10:00:46 +0100

    added display responsetime in swagger ui quarkus

--- lia_quarkus/src/main/java/org/acme/config/SwaggerConfig.java
+++ lia_quarkus/src/main/java/org/acme/config/SwaggerConfig.java
@@ -9,7 +9,11 @@ import javax.ws.rs.core.Application;
 @OpenAPIDefinition(
         info = @Info(
                 title = "Quarkus API", description = "This is a test project in quarkus", version = "1.0.0")
+
 )
 
 public class SwaggerConfig extends Application {
+
+
+
 }
